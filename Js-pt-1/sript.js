b0805596554a760253d7badc9c6fea6a5230d3fe
solobread12 <160695947+solobread12@users.noreply.github.com>
2026-01-21 13:24:05 +0700

    tugas-26-js

---
+++ Js-pt-1/sript.js
@@ -0,0 +1,9 @@
+// console.log("hello boss");
+// console.log("%chello, boss", "color: red; font-size: 40px;");
+// console.log(VariableYangTIdakAda);
+// console.error("ini adalah pesan eror yang ditampilkan manual");
+try {
+  let result = functionNull();
+} catch (error) {
+  console.error('terjadi kesalahan: ' + error.massage);
+}
---
+++ js-pt-2/boss.js
@@ -0,0 +1,3 @@
+// function greet() {
+//   alert("Hello, siswa kelas 9!");
+// }
